0663bf5f2b128d8eca9f260170dc9b8a131114d9
Ryo-exid <sakura4oneok@gmail.com>
2021-08-18 19:24:48 +0900

    変数の初期化（変数に初期値を代入）/再代入/定数

--- src/Chapter04.java
+++ src/Chapter04.java
@@ -1,16 +1,12 @@
 
 public class Chapter04 {
 	public static void main(String[] args) {
-		String name;
-		name = "Ryo";
+		final int LUCKY_NUMBER = 7;
+		String name = "Ryo";
 		System.out.println(name);
 		
 		name = "gon";
-
-		final int number;
-		number = 6;
-		
 		System.out.println(name);
-		System.out.println(number);
+		System.out.println(LUCKY_NUMBER);
 	}
 }

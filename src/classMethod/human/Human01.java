a7ca4d3c5c2e6a37b2d01b3d70f9ffe246192ad3
Ryo-exid <sakura4oneok@gmail.com>
2021-08-19 00:55:50 +0900

    クラスの基本構文/コンストラクタ/this変数

--- src/classMethod/human/Human01.java
+++ src/classMethod/human/Human01.java
@@ -1,18 +1,22 @@
 package classMethod.human;
 
+// アクセス修飾子publicを用いてどこからでもアクセスできるクラスHuman01を定義
 public class Human01 {
+	// アクセス修飾子publicを用いてどこからでもアクセスできるメンバー変数nameとageを定義
 	public String name; // メンバー変数
 	public int age; // メンバー変数
 	
-	// 引数なしのコンストラクタ
+	// 引数なしのコンストラクタを定義。これを用いて初期化した場合、nameは「山田」、ageは「20」という値で初期化される(それぞれ変数にセットされる)
 	public Human01() {
 		name = "山田";
 		age = 20;
 	}
 	
-	// 引数2つのコンストラクタ
+	// 引数2つのコンストラクタを定義
 	public Human01(String name, int age) {
-		this.name = "山田";
+		this.name = "山田"; // this変数(自分自身のインスタンスを指す変数を扱う)
 		this.age = 20;
 	}
 }
+
+// ClassMethod01.javaで実行

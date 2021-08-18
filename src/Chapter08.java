13431be61b81a5c84e5d9febe3332ec8b1b1951d
Ryo-exid <sakura4oneok@gmail.com>
2021-08-19 00:27:48 +0900

    while文/for文/拡張for文

--- src/Chapter08.java
+++ src/Chapter08.java
@@ -1,18 +1,22 @@
 
 public class Chapter08 {
 	public static void main(String[] args) {
+		// 1以上5未満の数の2乗を出力
 		int num = 1;
 		while (num < 5) {
 			System.out.println(num * num);
 			num++;
 		}
 		
-		int array[] = { 1, 4, 14, 25 };
+		int array[] = { 1, 4, 14, 25 }; // 要素数が4の「int」配列を初期化
+		// 配列のサイズ分ループして、要素を出力
 		for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
 		
+		// 拡張「for」文
 		for (int val : array) {
+			// 要素の値が偶数の場合は、continue文で処理をスキップ、奇数の場合は出力
             if (val % 2 == 0) {
                 continue;
             }

d186cd2fcb6e2603d352f627f0de5557f3f27313
Ryo-exid <sakura4oneok@gmail.com>
2021-08-18 19:18:56 +0900

    定数

---
+++ src/Variable02.java
@@ -0,0 +1,8 @@
+
+public class Variable02 {
+	public static void main(String[] args) {
+		// final修飾子を記述することで定数になる
+		final int MIN_NUMBER = 10; // 定数は全て大文字で記述(単語の区切りは_)
+		System.out.println(MIN_NUMBER);
+	}
+}

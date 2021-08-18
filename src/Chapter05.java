1ce58976730f195d98aab91e05a7f1692011c833
Ryo-exid <sakura4oneok@gmail.com>
2021-08-18 20:01:58 +0900

    プリミティブ型/ラッパークラス型/オートボクシング/配列

--- src/Chapter05.java
+++ src/Chapter05.java
@@ -1,27 +1,27 @@
 
 public class Chapter05 {
 	public static void main(String[] args) {
-		long num1;
-		num1 = 123456L;
+		// プリミティブ型の「long」「double」「boolean」の変数に値を代入して出力
+		long num1 = 123456L;
 		System.out.println(num1);
 		
-		double num2;
-		num2 = 3.1415;
+		double num2 = 3.14;
 		System.out.println(num2);
 		
-		boolean num3;
-		num3 = true;
+		boolean num3 = true;
 		System.out.println(num3);
 		
-		Float num4 = 1.5f;
+		// ラッパークラス型の「Float」「Integer」「Character」の変数に値を代入して出力（プリミティブ型をラッパークラス型に変えることをボクシングという）
+		Float num4 = 1.4142f; // ラッパークラスをオートボクシング（メソッドを使って明示的に呼び出さなくても変換が実行されること）で初期化して出力
 		System.out.println(num4);
 		
 		Integer num5 = 25;
 		System.out.println(num5);
 		
-		Character num6 = 'あ';
-		System.out.println(num6);
+		Character c = 'あ';
+		System.out.println(c);
 		
+		// 要素数が5の「int」配列に値を代入して、インデックス番号が3の値を出力
 		int[] array = { 0, 10, 20, 30, 40 };
 		System.out.println(array[3]);
 	}

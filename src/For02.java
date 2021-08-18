2a9a3e747062565d226b072b2230cfd56dbf81bb
Ryo-exid <sakura4oneok@gmail.com>
2021-08-19 00:12:37 +0900

    拡張for文

--- src/For02.java
+++ src/For02.java
@@ -1,7 +1,6 @@
 
 public class For02 {
 	public static void main(String[] args) {
-		// 拡張for文：
         // 配列の要素を順番にnumberに代入し、要素分繰り返す
         int[] array = { 1, 2, 3, 4, 5 };
         for (int number : array) {
@@ -10,6 +9,8 @@ public class For02 {
         }
     }
 }
+
+//拡張for文とは、配列やコレクションの全要素に対して、繰り返し処理を行なう制御文
 
 // for (型 変数名：配列やコレクションの変数) {
 //     ブロック(繰り返し処理)

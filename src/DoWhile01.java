ee1e259a945eb3eb7eea1cd1395db0354a833fc7
Ryo-exid <sakura4oneok@gmail.com>
2021-08-19 00:04:28 +0900

    do-while文

--- src/DoWhile01.java
+++ src/DoWhile01.java
@@ -1,12 +1,13 @@
 
 public class DoWhile01 {
     public static void main(String[] args) {
-        int number = 1;
+        int number = 100;
+        // 条件がfalseでも先にブロックを実行
         do {
             number *= 2;
             System.out.println("DoWhile01 = " + number);
         } while (number < 50);
     }
 }
-//仮にはじめから条件式の結果が「false」となっていても、while文とは異なり、最低1度はブロックの処理が実行される。
-//「While01.java」と「DoWhile01.java」で、変数の初期化を「int number = 100;」に変更して実行すると違いがわかる。
+// 仮にはじめから条件式の結果が「false」となっていても、while文とは異なり、最低1度はブロックの処理が実行される。
+// 「While01.java」と「DoWhile01.java」で、変数の初期化を「int number = 100;」に変更して実行すると違いがわかる。 => DoWhile01 = 200

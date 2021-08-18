7a234db5a6d6793a02ff8d9ffa68822a7c86537e
Ryo-exid <sakura4oneok@gmail.com>
2021-08-18 23:56:34 +0900

    if文/switch文

--- src/Chapter07.java
+++ src/Chapter07.java
@@ -1,9 +1,9 @@
 
 public class Chapter07 {
 	public static void main(String[] args) {
-		int distance = 10;
+		int distance = 10; // 距離を表すint型の変数を定義し、任意の値で初期化
 		if (distance <= 5) {
-			System.out.println("とても近いです");
+			System.out.println("とても近いです"); // 距離が5km以下の場合は "とても近いです" を出力
 		} else if (distance <= 10) {
 			System.out.println("近いです");
 		} else if (distance <= 15) {
@@ -12,10 +12,10 @@ public class Chapter07 {
 			System.out.println("とても遠いです");
 		}
 		
-		String color = "yellow";
+		String color = "yellow"; // 信号の色を表すString型の変数を定義し、 red/yellow/blueのいずれかで初期化
 		switch(color) {
 		case "red":
-			System.out.println("赤信号です");
+			System.out.println("赤信号です"); // 色がredの場合は "赤信号です" を出力
 			break;
 		case "yellow":
 			System.out.println("黄信号です");

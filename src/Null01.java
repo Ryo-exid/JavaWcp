822edfa17ea40253267850e6a701d8f93c806577
Ryo-exid <sakura4oneok@gmail.com>
2021-08-18 19:40:21 +0900

    型の基本/nullの変数を参照するとエラーになる

--- src/Null01.java
+++ src/Null01.java
@@ -3,12 +3,13 @@ public class Null01 {
     public static void main(String[] args) {
         String string1;
         // ”あいう”の文字数を出力
-        string1 = "あいう";
+        string1 = "あいう"; // 文字列をプログラム上に記述する場合はダブルクォーテーションを用いる
         System.out.println(string1 + "の文字数：" + string1.length());
         // ””（空文字）の文字数を出力
         string1 = "";
         System.out.println(string1 + "の文字数：" + string1.length());
-//        // nullの変数を参照するとNullPointerExceptionのエラー
+        
+//        // 以下、nullの変数を参照するとNullPointerExceptionのエラーが出力される処理
 //        string1 = null;
 //        System.out.println(string1 + "の文字数：" + string1.length());
     }

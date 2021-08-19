106e37ef2171329ea288fe7c036409c0c4b955e1
Ryo-exid <sakura4oneok@gmail.com>
2021-08-19 22:19:02 +0900

    余計なコメントを削除

--- src/classMethod/ClassMethod01.java
+++ src/classMethod/ClassMethod01.java
@@ -4,7 +4,7 @@ import classMethod.human.Human01;
 
 public class ClassMethod01 {
 	public static void main(String[] args) {
-		Human01 yamada = new Human01(); // 上記インスタンスのnameとageを表示すると、コンストラクタで代入した初期値が表示される
+		Human01 yamada = new Human01(); // インスタンスのnameとageを表示すると、コンストラクタで代入した初期値が表示される
 		System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です。"); // 上記インスタンスのnameとageを表示すると、コンストラクタで代入した初期値が表示される
 		
 		Human01 sato = new Human01("佐藤", 25); // 引数2つのコンストラクタを用いてHuman01クラスのインスタンスを生成

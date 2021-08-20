c15f599e0fd9afbdb29af6c4509d11ce3546ab70
Ryo-exid <sakura4oneok@gmail.com>
2021-08-20 20:48:41 +0900

    カプセル化/getter・setterメソッド

--- src/capsule/human/HumanCapsule.java
+++ src/capsule/human/HumanCapsule.java
@@ -1,6 +1,7 @@
 package capsule.human;
 
 public class HumanCapsule {
+	// privateを設定し、外部から隠蔽
 	private String name;
     private int age;
 
@@ -8,16 +9,32 @@ public class HumanCapsule {
         this.name = name;
         this.age = age;
     }
-
+    
+    // nameのgetterメソッド（privateされた値を取得するため）
     public String getName() {
         return this.name;
     }
-
+    
+    // nameのsetterメソッド（privateされた値を変更するため）
     public void setName(String name) {
         this.name = name;
     }
-
+    
+    // ageのgetterメソッド（ageは外部から変更禁止なので、setterメソッドは作成しない）
     public int getAge() {
         return this.age;
     }
 }
+
+
+// 基本構文（カプセル化）
+
+// getterメソッド
+// public 戻り値の型(フィールドの型)  getフィールド名() {
+//     return this.フィールド名
+// }
+
+// setterメソッド
+// public void setフィールド名(フィールドの型の引数) {
+//    this.フィールド名 = フィールドの型の引数;
+// }

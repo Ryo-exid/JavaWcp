1bf7f7189915c4450d568dbc22b9522142764071
Ryo-exid <sakura4oneok@gmail.com>
2021-08-20 21:23:59 +0900

    カプセル化したプログラムに変更

--- src/practice/Chapter12.java
+++ src/practice/Chapter12.java
@@ -2,10 +2,10 @@ package practice;
 
 public class Chapter12 {
 	public static void main(String[] args) {
-		NoCapsuleMan man = new NoCapsuleMan("瀬戸口", 24);
-		System.out.println("名前は" + man.name + "です。");
-		System.out.println("年齢は" + man.age + "歳です。");
-		man.age = 25;
-		System.out.println("誕生日を迎えたので、年齢が" + man.age + "歳になりました。");
+		CapsuleMan man = new CapsuleMan("瀬戸口", 24);
+		System.out.println("名前は" + man.getName() + "です。");
+		System.out.println("年齢は" + man.getAge() + "歳です。");
+		man.setAge(25);
+		System.out.println("誕生日を迎えたので、年齢が" + man.getAge() + "歳になりました。");
 	}
 }

33119845c8c1c93c21379965857aec01d0d5b30a
Ryo-exid <sakura4oneok@gmail.com>
2021-08-20 18:19:56 +0900

    例外処理/try-catch-finally文

--- src/exception/Exception02.java
+++ src/exception/Exception02.java
@@ -7,11 +7,11 @@ public class Exception02 {
             System.out.println("100 ÷ 0 は？");
             int result = 100 / 0;
             System.out.println("計算結果" + result);
-        } catch (ArithmeticException e) {
-        	// catchブロック（例外が発生した時に実行する処理）
+        } catch (ArithmeticException e) { // ArithmeticExceptionクラスの例外が発生した場合の例外処理
+        	// 例外クラスが一致するので、例外処理が実行される
             System.out.println("例外が発生");
         } finally {
-        	// finallyブロック(例外発生の有無にかかわらず必ず実行する処理)
+        	// 例外発生の有無にかかわらず必ず実行
             System.out.println("プログラム終了");
         }
     }

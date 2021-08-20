e16d2757439a74ee8c4544f4b5d6a5fb462548c1
Ryo-exid <sakura4oneok@gmail.com>
2021-08-20 18:31:21 +0900

    throw文（意図的に例外を発生させたい場合に使用）

--- src/exception/Exception03.java
+++ src/exception/Exception03.java
@@ -3,7 +3,7 @@ package exception;
 public class Exception03 {
     public static void main(String[] args) {
         try {
-            // divisionメソッドを呼び出し
+            // divisionメソッド（割り算）を呼び出し
             Exception03.division(100, 0);
         // ArithmeticExceptionクラスの例外が発生した場合の例外処理
         } catch (ArithmeticException e) {
@@ -13,7 +13,7 @@ public class Exception03 {
         } catch (IllegalArgumentException e) {
             // 例外処理が実行される
             System.out.println("IllegalArgumentException例外が発生");
-            // キャッチした例外インスタンスをスロー
+            // キャッチした例外インスタンスをthrow（意図的に例外を発生）
             throw e;
         }
         // finallyブロックでないので処理が実行されない
@@ -23,10 +23,17 @@ public class Exception03 {
     public static void division(int a, int b) {
         System.out.println(a + " ÷ " + b + " は？");
         if (b == 0) {
-            // 割り算で例外が発生するので、意図的にIllegalArgumentException例外をスロー
+            // 割り算で例外が発生するので、意図的にIllegalArgumentException例外をthrow（意図的に例外を発生）
             throw new IllegalArgumentException("引数が不正です");
         }
         int result = a / b;
         System.out.println("計算結果" + result);
     }
 }
+
+
+// 例外インスタンスをスロー
+// throw 例外インスタンス;
+
+// 例外インスタンスを生成してスロー
+// throw new 例外クラス;

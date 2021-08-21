c145e4b80c51cbeba1729c66273320c05ba16b83
Ryo-exid <sakura4oneok@gmail.com>
2021-08-21 18:39:53 +0900

    継承/コメントを補足

--- src/extend/human/Employee.java
+++ src/extend/human/Employee.java
@@ -1,15 +1,17 @@
 package extend.human;
 
+//Humanクラスを継承したEmployeeクラス
 public class Employee extends Human {
-	private String department;
+	private String department; // サブクラスで追加したフィールド
 	
 	public Employee(String name, int age, String department) {
-		super(name, age);
+		super(name, age); // スーパークラスのコンストラクタを呼び出し
 		this.department = department;
 	}
 	
+	// サブクラスで追加したメソッド
 	public String getEmployeeProfile() {
-		String profile = "年齢は" + this.age + "歳です。";
+		String profile = "年齢は" + this.age + "歳です。"; // 親インスタンスのフィールドをthis変数で参照
 		profile += "サラリーマンで、部署は" + this.department + "です。";
 		return profile;
 	}

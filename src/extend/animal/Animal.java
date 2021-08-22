a05b89ed4ec198b8ef334dbfc55f4519b3ca3104
Ryo-exid <sakura4oneok@gmail.com>
2021-08-22 21:00:17 +0900

    抽象クラス

--- src/extend/animal/Animal.java
+++ src/extend/animal/Animal.java
@@ -1,5 +1,12 @@
 package extend.animal;
 
-public class Animal {
+public abstract class Animal {
+	protected String name;
+	public void eat() {
+		System.out.println(name + "が食事をしました！");
+	}
 	
+	public abstract void cry();
 }
+
+// 抽象クラスとは、抽象メソッドを1つ以上持つクラスのこと

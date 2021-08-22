1fe3bf54298ac2a18aab2af215e06d04705f3257
Ryo-exid <sakura4oneok@gmail.com>
2021-08-22 21:15:43 +0900

    抽象クラス/コメントを補足

--- src/extend/Abstract01.java
+++ src/extend/Abstract01.java
@@ -5,9 +5,12 @@ import extend.animal.Dog;
 public class Abstract01 {
 	public static void main(String[] args) {
 		
-		Dog dog = new Dog();
-		dog.eat();
-		dog.cry();
+		Dog dog = new Dog(); // Dogクラスのインスタンスを生成
+		dog.eat(); // 抽象クラス（Animalクラス）のメソッドを呼び出し
+		dog.cry(); // 抽象メソッドを実装したメソッドを呼び出し
+		
+		// 抽象クラスはインスタンス生成するとコンパイルエラー
+        // Animal animal = new Animal();
 	}
 }
 

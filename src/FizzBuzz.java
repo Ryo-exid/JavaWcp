26f214e4bb95513db9952ad5c45f2ff6eac27e18
Ryo-exid <sakura4oneok@gmail.com>
2021-08-23 23:02:16 +0900

    FizzBuzz/変数の宣言

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,4 +1,16 @@
 
 public class FizzBuzz {
-
+	public static void main(String[] args) {
+		int i = 0;
+		
+		int loopCount = 100;
+		
+		int numFuzz = 3;
+		
+		int numBuzz = 5;
+		
+		String strFizz = "Fizz";
+		String strBuzz = "Buzz";
+		
+	}
 }

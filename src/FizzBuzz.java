51c3b26f0b91cc627f2198cda4d62fc2dd33cdb3
Ryo-exid <sakura4oneok@gmail.com>
2021-08-24 10:56:00 +0900

    FizzBuzz/コメントを補足

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,16 +1,14 @@
 
 public class FizzBuzz {
 	public static void main(String[] args) {
+		// 変数宣言
 		int i = 0;
-		
 		int loopCount = 100;
-		
-		int numFizz = 3;
-		
+		int numFizz = 3;	
 		int numBuzz = 5;
 		
-		String strFizz = "Fizz";
-		String strBuzz = "Buzz";
+		String strFizz = "Fizz"; // Fizz文字列
+		String strBuzz = "Buzz"; // Buzz文字列
 		
 		for (i = 1; i <= loopCount; i++) {
 			if (i % (numFizz * numBuzz) == 0) {

2b45970b15052f5a5b003705f39f9101ec72d6d1
Ryo-exid <sakura4oneok@gmail.com>
2021-08-24 22:28:26 +0900

    奇数判定プログラム

--- src/Odd_Number.java
+++ src/Odd_Number.java
@@ -1,4 +1,12 @@
 
 public class Odd_Number {
-
+	public static void main(String[] args) {
+		// 0〜100までの繰り返し
+		for (int i = 0; i <= 100; i++) {
+			// 2で割ったあまりが0以外
+			if (i % 2 != 0) {
+				System.out.println(i + "は奇数です。");
+			}
+		}
+	}
 }

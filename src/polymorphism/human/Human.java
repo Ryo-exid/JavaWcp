1cea9324c6550a38f4352e7eeecfbe506c6936a4
Ryo-exid <sakura4oneok@gmail.com>
2021-08-26 17:47:24 +0900

    ポリモーフィズム

---
+++ src/polymorphism/human/Human.java
@@ -0,0 +1,24 @@
+package polymorphism.human;
+
+public class Human {
+	private String name;
+	protected int age;
+	
+	public Human(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	// final修飾子（一度しか値を代入できない）
+	final public String getName() {
+		return this.name;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public String getProfile() {
+		return "名前は" + this.name + "、年齢は" + this.age + "です。";
+	}
+}

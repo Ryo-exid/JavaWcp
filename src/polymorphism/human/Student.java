0d3a82a8597c9c83da8e59e71293e0fe0b336b65
Ryo-exid <sakura4oneok@gmail.com>
2021-08-26 18:09:40 +0900

    @Override

--- src/polymorphism/human/Student.java
+++ src/polymorphism/human/Student.java
@@ -8,9 +8,20 @@ public class Student extends Human {
 		this.score = score;
 	}
 	
-	public String getprofile() {
+	// オーバーライドして、Studentクラス用に処理を変更
+	@Override
+	public String getProfile() {
 		String profile = "年齢は" + super.age + "です。";
 		profile += "学生で、テストの点数は" + this.score + "点です。";
 		return profile;
 	}
 }
+
+// サブクラス側のメソッドの直前の行に @Override と書くことで、下記すべてを満たしていることをコンパイル時に保証できる。
+// ・メソッド名が同じである
+// ・引数の型と数が同じである
+// ・戻り値の型が同じか、スーパークラス・サブクラスの関係にある
+// ・例外の型が同じか、スーパークラス・サブクラスの関係にある
+// ・アクセス修飾子が同じか、スーパークラスの制約の方が厳しい
+
+// @Overrideを書いておけば、1つでも欠けている条件があるとコンパイルエラーとなる。

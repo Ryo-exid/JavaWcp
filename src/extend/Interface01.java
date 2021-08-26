62caa491bfce3993a46f9c0bd3862557260214e3
Ryo-exid <sakura4oneok@gmail.com>
2021-08-26 16:12:38 +0900

    インターフェース/コメント補足

--- src/extend/Interface01.java
+++ src/extend/Interface01.java
@@ -5,14 +5,18 @@ import extend.phone.MobilePhone;
 
 public class Interface01 {
 	public static void main(String[] args) {
+		// MobilePhoneインスタンスを生成
 		MobilePhone mobilePhone = new MobilePhone("000-1111-2222");
 		
-		System.out.println("電話番号は最大" + MobilePhone.MAX_NUMBER_DIGITS + "桁です。");
-	    mobilePhone.call("99-8888-7777");
-	    Interface01.classTakePicture(mobilePhone);	    
+		System.out.println("電話番号は最大" + MobilePhone.MAX_NUMBER_DIGITS + "桁です。"); // Phoneインターフェースのフィールド（定数）を参照
+	    mobilePhone.call("99-8888-7777"); // Phoneインターフェースの抽象メソッドを実装したメソッドを呼び出し
+	    Interface01.classTakePicture(mobilePhone); // classTakePictureメソッドの呼び出し
 	}
 	
+	// Cameraインターフェース実装したクラスは呼び出しできる
 	public static void classTakePicture(Camera camera) {
+		
+		// アップキャストされた変数のメソッドを呼び出し
 		camera.takePicture();
 	}
 }

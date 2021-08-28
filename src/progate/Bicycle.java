79a4e5cba331d568ec096dc8c9e5b876710d2356
Ryo-exid <sakura4oneok@gmail.com>
2021-08-28 20:24:25 +0900

    Bicycleクラスにdistanceを追加

--- src/progate/Bicycle.java
+++ src/progate/Bicycle.java
@@ -4,6 +4,7 @@ public class Bicycle {
 	// フィールド
 	private String name;
 	private String color;
+	private int distance = 0;
 	
 	// コンストラクタ
 	Bicycle(String name, String color) {
@@ -15,5 +16,8 @@ public class Bicycle {
 	public void printData() {
 		System.out.println("名前：" + this.name);
 		System.out.println("色：" + this.color);
+		System.out.println("走行距離：" + this.distance + "km");
 	}
+	
+	
 }

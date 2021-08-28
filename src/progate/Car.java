205177539cb2314db159c1fca676eeae4f5541bb
Ryo-exid <sakura4oneok@gmail.com>
2021-08-28 21:27:49 +0900

    charge（給油）メソッドを追加

--- src/progate/Car.java
+++ src/progate/Car.java
@@ -5,6 +5,7 @@ public class Car {
 	private String color;
 	private int distance = 0; // 走行距離、初期値0（km）
 	private int fuel = 100; // ガソリン量、初期値100（L）
+	private int litre; // 給油量
 	
 	Car(String name, String color) {
 		this.name = name;
@@ -27,6 +28,19 @@ public class Car {
 			System.out.println("ガソリンが足りません");
 		}
 		System.out.println("走行距離：" + this.distance + "km");
+		System.out.println("ガソリン量：" + this.fuel + "L");
+	}
+	
+	public void charge(int litre) {
+		System.out.println(litre + "L給油します");
+		if (litre <= 0) { // 給油量が0以下の場合
+			System.out.println("給油できません");
+		} else if (litre + this.fuel >= 100) { // 給油量とガソリン量の合計が100以上の場合
+			System.out.println("満タンまで給油します");
+			this.fuel = 100; // ガソリン量を満タン（初期値の100）に戻す
+		} else { // 給油量が0Lより多く、満タン未満の場合
+			this.fuel += litre; // ガソリン量の値を更新（給油した分増やす）
+		}
 		System.out.println("ガソリン量：" + this.fuel + "L");
 	}
 }

af93a271e481908af23812cfac4eff55dd70852c
Ryo-exid <sakura4oneok@gmail.com>
2021-08-28 20:18:34 +0900

    自転車の情報（名前・色）を出力

--- src/progate/Bicycle.java
+++ src/progate/Bicycle.java
@@ -1,5 +1,19 @@
 package progate;
 
 public class Bicycle {
-
+	// フィールド
+	private String name;
+	private String color;
+	
+	// コンストラクタ
+	Bicycle(String name, String color) {
+		this.name = name;
+		this.color = color;
+	}
+	
+	// メソッド
+	public void printData() {
+		System.out.println("名前：" + this.name);
+		System.out.println("色：" + this.color);
+	}
 }
--- src/progate/Dojo2.java
+++ src/progate/Dojo2.java
@@ -1,5 +1,9 @@
 package progate;
 
 public class Dojo2 {
-
+	public static void main(String[] args) {
+		Bicycle bicycle = new Bicycle("ビアンキ", "赤");
+		System.out.println("【自転車の情報】");
+		bicycle.printData();
+	}
 }

081bec32e5fe90467e80417735aacca6e43498ff
Ryo-exid <sakura4oneok@gmail.com>
2021-08-28 20:51:59 +0900

    fuelフィールドを追加

--- src/progate/Car.java
+++ src/progate/Car.java
@@ -3,7 +3,8 @@ package progate;
 public class Car {
 	private String name;
 	private String color;
-	private int distance;
+	private int distance = 0; // 走行距離、初期値0(km)
+	private int fuel = 100; // ガソリン量、初期値100(L)
 	
 	Car(String name, String color) {
 		this.name = name;
@@ -14,6 +15,7 @@ public class Car {
 		System.out.println("名前：" + this.name);
 		System.out.println("色：" + this.color);
 		System.out.println("走行距離：" + this.distance + "km");
+		System.out.println("ガソリン量：" + this.fuel + "L");
 	}
 	
 	public void run(int distance) {

1380de53a4883101b4ae325b99b75971f97fec29
Ryo-exid <sakura4oneok@gmail.com>
2021-08-28 20:36:19 +0900

    走行距離を入力できるように変更

--- src/progate/Dojo2.java
+++ src/progate/Dojo2.java
@@ -1,11 +1,17 @@
 package progate;
 
+import java.util.Scanner;
+
 public class Dojo2 {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
 		Bicycle bicycle = new Bicycle("ビアンキ", "赤");
 		System.out.println("【自転車の情報】");
 		bicycle.printData();		
 		System.out.println("-----------------");
-		bicycle.run(10);
+		System.out.print("走る距離を入力してください：");
+		int bicycleDistance = scanner.nextInt();
+		bicycle.run(bicycleDistance);
 	}
 }

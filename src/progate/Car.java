4818d3e2487c3e70e5a936b0829be1fcf29c870c
Ryo-exid <sakura4oneok@gmail.com>
2021-08-28 21:03:18 +0900

    コンソールから受け取った走行距離に合わせてガソリン量の値を更新

--- src/progate/Car.java
+++ src/progate/Car.java
@@ -3,8 +3,8 @@ package progate;
 public class Car {
 	private String name;
 	private String color;
-	private int distance = 0; // 走行距離、初期値0(km)
-	private int fuel = 100; // ガソリン量、初期値100(L)
+	private int distance = 0; // 走行距離、初期値0（km）
+	private int fuel = 100; // ガソリン量、初期値100（L）
 	
 	Car(String name, String color) {
 		this.name = name;
@@ -20,7 +20,13 @@ public class Car {
 	
 	public void run(int distance) {
 		System.out.println(distance + "km走ります");
-		this.distance += distance;
+		if (distance <= this.fuel) { // 走行距離がガソリン量以下の場合
+			this.distance += distance; // 走行距離更新
+			this.fuel -= distance; // ガソリン量更新（1kmごとに1L減るという仮定）
+		} else {
+			System.out.println("ガソリンが足りません");
+		}
 		System.out.println("走行距離：" + this.distance + "km");
+		System.out.println("ガソリン量：" + this.fuel + "L");
 	}
 }

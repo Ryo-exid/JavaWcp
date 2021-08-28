464882b91695c2679444e9e75d2abf02584c4169
Ryo-exid <sakura4oneok@gmail.com>
2021-08-28 20:31:47 +0900

    走行距離を表示するメソッドを追加

--- src/progate/Dojo2.java
+++ src/progate/Dojo2.java
@@ -4,6 +4,8 @@ public class Dojo2 {
 	public static void main(String[] args) {
 		Bicycle bicycle = new Bicycle("ビアンキ", "赤");
 		System.out.println("【自転車の情報】");
-		bicycle.printData();
+		bicycle.printData();		
+		System.out.println("-----------------");
+		bicycle.run(10);
 	}
 }

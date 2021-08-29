3ec2380216f820699269e57d2107c5893813468f
Ryo-exid <sakura4oneok@gmail.com>
2021-08-29 15:30:54 +0900

    継承/親クラスにCarとBicycleの共通処理を記述

--- src/progate/V/Vehicle.java
+++ src/progate/V/Vehicle.java
@@ -1,5 +1,34 @@
 package progate.V;
 
+// CarとBicycleの共通処理を記述
 public class Vehicle {
-
+	private String name;
+	private String color;
+	private int distance;
+	
+	public String getName() {
+		return this.name = name;
+	}
+	
+	public String getColor() {
+		return this.color = color;
+	}
+	
+	public int getDistance() {
+		return this.distance = distance;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public void printData() {
+		System.out.println("名前：" + this.name);
+		System.out.println("色：" + this.color);
+		System.out.println("走行距離：" + this.distance + "km");
+	}
 }

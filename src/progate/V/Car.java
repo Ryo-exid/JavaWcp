51c97a004d17e855e747c8666570c482d03db474
Ryo-exid <sakura4oneok@gmail.com>
2021-08-29 15:55:46 +0900

    サブクラス独自のフィールドとメソッドを定義

--- src/progate/V/Car.java
+++ src/progate/V/Car.java
@@ -1,5 +1,23 @@
 package progate.V;
 
 public class Car extends Vehicle {
+	private int fuel = 50; // Carクラスの独自のフィールドを追加
 	
+	public int getFuel() {
+		return this.fuel;
+	}
+	
+	// Carクラス独自のメソッド
+	public void charge(int litre) {
+	    System.out.println(litre + "L給油します");
+	    if (litre <= 0) {
+	      System.out.println("給油できません");
+	    } else if (litre + this.fuel >= 100) {
+	      System.out.println("満タンまで給油します");
+	      this.fuel = 100;
+	    } else {
+	      this.fuel += litre;
+	    }
+	    System.out.println("ガソリン量：" + this.fuel + "L");
+	  }
 }

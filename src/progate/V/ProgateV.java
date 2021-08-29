20ecc5aeb1db69c5c50495b3da5489063353959c
Ryo-exid <sakura4oneok@gmail.com>
2021-08-29 17:34:04 +0900

    人が乗り物を所有できるようにする/セッター・ゲッターの呼び出し

--- src/progate/V/ProgateV.java
+++ src/progate/V/ProgateV.java
@@ -4,9 +4,19 @@ import java.util.Scanner;
 
 public class ProgateV {
 	public static void main(String[] args) {
+		Person person1 = new Person("Kate", "Jones", 27, 1.6, 50.0);
+	    Person person2 = new Person("John", "Christopher", "Smith", 65, 1.75, 80.0);
+	    
 		Scanner scanner = new Scanner(System.in);
 		Car car = new Car("フェラーリ", "赤");	
+		
+		// carの所有者をperson1に設定
+		car.setOwner(person1);
+		
 		Bicycle bicycle = new Bicycle("ビアンキ", "緑");
+		
+		// bicycleの所有者をperson2に設定
+		bicycle.setOwner(person2);
 		
 		System.out.println("【車の情報】");
 		car.printData();
@@ -14,12 +24,15 @@ public class ProgateV {
 		System.out.print("走る距離を入力してください：");
 		int carDistance = scanner.nextInt();
 		car.run(carDistance);
-		
-		
+			
 		System.out.println("-----------------");
 	    System.out.print("給油する量を入力してください：");
 	    int litre = scanner.nextInt();
 	    car.charge(litre);
+	    
+	    System.out.println("-----------------");
+	    System.out.println("【車の所有者の情報】");
+	    car.getOwner().printData();
 		
 		System.out.println("=================");
 		System.out.println("【自転車の情報】");
@@ -28,5 +41,9 @@ public class ProgateV {
 		System.out.print("走る距離を入力してください：");
 		int bicycleDistance = scanner.nextInt();
 		bicycle.run(bicycleDistance);
+		
+		System.out.println("-----------------");
+	    System.out.println("【自転車の所有者の情報】");
+	    bicycle.getOwner().printData();
 	}
 }

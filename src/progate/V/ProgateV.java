638b4bc397d7650177df888aa1fff87f1d8c53df
Ryo-exid <sakura4oneok@gmail.com>
2021-08-29 16:21:33 +0900

    スーパークラスのコンストラクタを呼び出す

--- src/progate/V/ProgateV.java
+++ src/progate/V/ProgateV.java
@@ -5,13 +5,8 @@ import java.util.Scanner;
 public class ProgateV {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		Car car = new Car();
-		car.setName("フェラーリ");
-		car.setColor("赤");
-		
-		Bicycle bicycle = new Bicycle();
-		bicycle.setName("ビアンキ");
-		bicycle.setColor("緑");
+		Car car = new Car("フェラーリ", "赤");	
+		Bicycle bicycle = new Bicycle("ビアンキ", "緑");
 		
 		System.out.println("【車の情報】");
 		car.printData();

ed6c7e2934f32ae06e4a9f55f94fee9e18a827cf
Ryo-exid <sakura4oneok@gmail.com>
2021-08-29 16:37:56 +0900

    runメソッドを定義

--- src/progate/V/Bicycle.java
+++ src/progate/V/Bicycle.java
@@ -4,4 +4,10 @@ public class Bicycle extends Vehicle {
 	Bicycle(String name, String color) {
 		super(name, color); // スーパークラスのコンストラクタを呼び出す
 	}
+	
+	public void run(int distance) {
+		System.out.println(distance + "km走ります");
+		this.distance += distance;
+		System.out.println("走行距離：" + this.distance + "km");
+	}
 }

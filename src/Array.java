1d1ce61135c117c0964aaae3bad152249eca98a3
Ryo-exid <sakura4oneok@gmail.com>
2021-08-30 11:01:09 +0900

    配列/Arrays.sort

--- src/Array.java
+++ src/Array.java
@@ -1,4 +1,12 @@
+import java.util.Arrays;
 
 public class Array {
-
+	public static void main(String[] args) {
+		int[] num = {3, 5, 1, 2, 4, 7, 6};
+		Arrays.sort(num); // 配列を昇順に並び替え
+		
+		for (int i = 0; i < num.length; i++) {
+			System.out.println(num[i]);
+		}
+	}
 }

f812ee86de42efaf8b879f3a333885b3fcb761e1
Ryo-exid <sakura4oneok@gmail.com>
2021-08-31 21:28:37 +0900

    スレッド

--- src/practice/Chapter15_4.java
+++ src/practice/Chapter15_4.java
@@ -1,7 +1,29 @@
+// スレッド（プログラムを処理する単位のこと）※Javaのプログラムはスレッドによって実行されている。
+
 package practice;
 
 public class Chapter15_4 {
 	public static void main(String[] args) {
+		for (int i = 1; i <= 2; i++) {
+			// Runnableインターフェースを実装したクラスをスレッドの実行（以下）
+			// ThreadSampleクラスのインスタンスを生成
+			ThreadSample mt = new ThreadSample(i);
+			// Threadクラスのインスタンスを作成
+			Thread thread = new Thread(mt);
+			// Threadクラスのstartメソッドを呼び出す
+			thread.start();
+		}
 		
+		for (int i = 1; i <= 3; i++) {
+			try {
+				// Thread.sleepは、指定したミリ秒間スリープするメソッド
+                Thread.sleep(1000);
+                // 1000ミリ秒（1秒）間スリープしてメッセージを表示
+                System.out.println("Chapter15_4のスレッド : " + i + "回目");
+             // Thread.sleepの例外処理
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+		}
 	}
 }

c7d6dc853d0485f22c62b21f9b4c2f7e45827fe3
Ryo-exid <sakura4oneok@gmail.com>
2021-09-01 12:39:14 +0900

    内部クラス/コメント補足

--- src/inner_class_sample/Outer.java
+++ src/inner_class_sample/Outer.java
@@ -4,3 +4,17 @@ public class Outer {
 	public class Inner {
 	}
 }
+
+// 内部クラス
+
+// staticクラスは、クラス外から直接インスタンス化できる
+// 非staticクラスは、外部クラスのインスタンスからのみインスタンス化できる
+
+
+
+// 内部クラスがstatic
+
+// public class Outer {
+//	   public static class Inner {
+//     }
+// }

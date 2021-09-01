4d8b3726851227297e0d95eba82ba39abc544579
Ryo-exid <sakura4oneok@gmail.com>
2021-09-01 12:46:40 +0900

    内部クラス/インスタンス化

--- src/inner_class_sample/Example.java
+++ src/inner_class_sample/Example.java
@@ -7,9 +7,9 @@ public class Example {
 		
 		// 内部クラスがstaticだった場合のインスタンス化
 		// Outer.Inner inner = new Outer.Inner();
-}
+	}
 }
 
 // staticな内部クラスBをインスタンス化する場合は、 new Outer.Inner() のように書くことで直接インスタンス化可能。
 // または、import inner_class_sample.Outer.Inner; とimport文を書けば
-// new Inner() のみでインスタンスを生成することが可能。
+// new Inner() のみでインスタンスを生成することが可能

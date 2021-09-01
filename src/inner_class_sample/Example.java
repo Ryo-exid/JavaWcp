75ed4f010a6d6f3da82541c951e0cb8b6ea9c6ad
Ryo-exid <sakura4oneok@gmail.com>
2021-09-01 12:39:19 +0900

    内部クラス/コメント補足

--- src/inner_class_sample/Example.java
+++ src/inner_class_sample/Example.java
@@ -3,6 +3,13 @@ package inner_class_sample;
 public class Example {
 	public static void main(String[] args) {
 		Outer outer = new Outer(); // クラスOuterをインスタンス化
-		Outer.Inner inner = outer.new Inner(); // // クラスOuterの内部クラスInnerをインスタンス化
-	}
+		Outer.Inner inner = outer.new Inner(); // クラスOuterの内部クラスInnerをインスタンス化（内部クラスが非staticな場合）
+		
+		// 内部クラスがstaticだった場合のインスタンス化
+		// Outer.Inner inner = new Outer.Inner();
 }
+}
+
+// staticな内部クラスBをインスタンス化する場合は、 new Outer.Inner() のように書くことで直接インスタンス化可能。
+// または、import inner_class_sample.Outer.Inner; とimport文を書けば
+// new Inner() のみでインスタンスを生成することが可能。

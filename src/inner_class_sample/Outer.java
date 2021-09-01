88804068eaa31c7eb189e273ac4485ea3b65b0e7
Ryo-exid <sakura4oneok@gmail.com>
2021-09-01 12:54:33 +0900

    内部クラスは、外部クラスのフィールドを直接参照することが出来る

--- src/inner_class_sample/Outer.java
+++ src/inner_class_sample/Outer.java
@@ -1,7 +1,14 @@
 package inner_class_sample;
 
 public class Outer {
+	// 外部クラスに定義したフィールドgreeting
+	private String greeting = "こんにちは";
+	
 	public class Inner {
+		public String hello() {
+			// 内部クラスInnerから、外部クラスOuterのフィールドgreetingを参照
+			return greeting;
+		}
 	}
 }
 

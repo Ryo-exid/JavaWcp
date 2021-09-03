6b817ff7d26e57437c83ab85fc5fb58f3e801859
Ryo-exid <sakura4oneok@gmail.com>
2021-09-03 14:34:15 +0900

    列挙型のメソッド/valueOf()

--- src/enumPractice/EnumPractice01.java
+++ src/enumPractice/EnumPractice01.java
@@ -8,7 +8,12 @@ public class EnumPractice01 {
 		System.out.println(Dessert.ICE_CREAM);
 		System.out.println(Dessert.CAKE);
 		
-		// 列挙した定数名そのものをString型で返すメソッドname()
+		// 列挙した定数名そのものをString型で返すメソッドname() ※列挙型のメソッド
 		System.out.println(Dessert.APPLE.name());
 	}
 }
+
+// valueOf()  ※name()とは逆に、String型の名称から列挙型のインスタンスを取得したい場合
+
+// Dessert apple = Dessert.valueOf("APPLE");  ※valueOf()に列挙されていない文字列を指定すると例外が発生するため注意
+// System.out.println(apple);

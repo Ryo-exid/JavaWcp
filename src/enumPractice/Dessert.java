d309d59581ef0ce2959adb59363c9074aa44b5d5
Ryo-exid <sakura4oneok@gmail.com>
2021-09-03 14:44:19 +0900

    列挙型/列挙したそれぞれのインスタンスにさらに値を持たせる

--- src/enumPractice/Dessert.java
+++ src/enumPractice/Dessert.java
@@ -1,7 +1,22 @@
 package enumPractice;
 
 public enum Dessert {
-	APPLE, ICE_CREAM, CAKE,
+	// それぞれに値段を指定する
+	APPLE(100), ICE_CREAM(200), CAKE(300),;
+	
+	// 値段を保持するフィールド
+	private int price;
+	
+	// コンストラクタを追加。引数はint型の値段。
+	private Dessert(int price) {
+		this.price = price;
+	}
+	
+	// 値段を取得するメソッドを追加
+	public int getPrice() {
+		return price;
+	}
+	
 }
 
 // Enum（列挙型）とは、プログラムの中で定義する定数を拡張し、さらに安全かつ便利にする仕組み

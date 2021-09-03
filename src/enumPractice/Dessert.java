32c959d34e39faf5f3f72f83baf1a6ddaf22a74e
Ryo-exid <sakura4oneok@gmail.com>
2021-09-03 14:22:33 +0900

    Enum（列挙型）/コメント補足

--- src/enumPractice/Dessert.java
+++ src/enumPractice/Dessert.java
@@ -3,3 +3,8 @@ package enumPractice;
 public enum Dessert {
 	APPLE, ICE_CREAM, CAKE,
 }
+
+// Enum（列挙型）とは、プログラムの中で定義する定数を拡張し、さらに安全かつ便利にする仕組み
+
+// public static final String APPLE = "りんご";
+// と、上記のような形よりも簡潔に書くことができる

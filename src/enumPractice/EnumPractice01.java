86f2b22d947acc834c79d0fae954fad838356702
Ryo-exid <sakura4oneok@gmail.com>
2021-09-03 14:22:55 +0900

    列挙した定数の呼び出し

--- src/enumPractice/EnumPractice01.java
+++ src/enumPractice/EnumPractice01.java
@@ -2,6 +2,8 @@ package enumPractice;
 
 public class EnumPractice01 {
 	public static void main(String[] args) {
+		
+		// Dessert.javaで列挙した定数の呼び出し
 		System.out.println(Dessert.APPLE);
 		System.out.println(Dessert.ICE_CREAM);
 		System.out.println(Dessert.CAKE);

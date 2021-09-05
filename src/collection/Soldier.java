d606da5353fcdfb82c6924851c3814b546c587d0
Ryo-exid <sakura4oneok@gmail.com>
2021-09-05 16:14:28 +0900

    コレクション/ポリモーフィズム

--- src/collection/Soldier.java
+++ src/collection/Soldier.java
@@ -1,7 +1,7 @@
 package collection;
 
 public class Soldier implements Character {
-	
+	// インターフェースのメソッドを「Soldier」用にオーバーライド
 	public int attack() {
 		System.out.println("戦士の会心の一撃！");
 		return 100;

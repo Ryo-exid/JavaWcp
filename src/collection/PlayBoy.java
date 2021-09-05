39c3282c6a50a73811b7243441635b05ab0b079c
Ryo-exid <sakura4oneok@gmail.com>
2021-09-05 16:14:18 +0900

    コレクション/ポリモーフィズム

--- src/collection/PlayBoy.java
+++ src/collection/PlayBoy.java
@@ -1,7 +1,7 @@
 package collection;
 
 public class PlayBoy implements Character{
-	
+	// インターフェースのメソッドを「Playboy」用オーバーライド
 	public int attack() {
 		System.out.println("遊び人は、ただ遊んでいるだけだった！");
 		return 0;

96b25326771901884c4f88058da904d6511fc2f0
Ryo-exid <sakura4oneok@gmail.com>
2021-09-06 20:27:04 +0900

    境界型パラメータ/籠とその中身の合計金額を出力するメソッドを追加

--- src/generics/Basket02.java
+++ src/generics/Basket02.java
@@ -1,8 +1,11 @@
 package generics;
 
+import java.math.BigDecimal;
+
 // Fruitのサブクラスしか受け取れないよう、extendsキーワードを用いて次のように定義
 class Basket02 <E extends Fruit> {
 	private E elem;
+	private final BigDecimal PRICE = new BigDecimal(100); // 籠代が100円（初期値）
 	
 	Basket02(E e) {
 		elem = e;
@@ -18,5 +21,10 @@ class Basket02 <E extends Fruit> {
 	
 	void printName() {
 		System.out.println("籠の中身は" + elem.getName() + "です"); // EはFruitのサブクラスなので、getNameが使えると保証されている
+	}
+	
+	void printTotalPrice() {
+		BigDecimal totalPrice = PRICE.add(elem.getPrice());
+		System.out.println(elem.getName() + "の入った籠は合計" + totalPrice + "円です");
 	}
 }

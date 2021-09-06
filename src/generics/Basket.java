134643f0d70d7d7a8fbd0c91bbfbffbb69c1a2f1
Ryo-exid <sakura4oneok@gmail.com>
2021-09-06 19:34:44 +0900

    ジェネリクスを使ったクラス

---
+++ src/generics/Basket.java
@@ -0,0 +1,19 @@
+package generics;
+
+public class Basket<E> {
+	private E elem;
+	
+	Basket(E e) {
+		elem = e ;
+	}
+	
+	void replace(E e) {
+		elem = e;
+	}
+	
+	E get() {
+		return elem;
+	}
+}
+
+// 慣習的に、型パラメータにはelement(要素)の Eやtype(型)の Tなど、大文字の英字1文字を用いることが多い

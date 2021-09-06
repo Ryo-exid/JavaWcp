db66925e5815f771de6d0923cc79d796ae21a896
Ryo-exid <sakura4oneok@gmail.com>
2021-09-06 20:24:42 +0900

    境界型パラメータ/価格用のフィールドとメソッドを追加

--- src/generics/Orange.java
+++ src/generics/Orange.java
@@ -1,12 +1,19 @@
 package generics;
 
+import java.math.BigDecimal;
+
 class Orange extends Fruit {
-	Orange(String name) {
-		super(name);
+	Orange(String name, BigDecimal price) {
+		super(name, price);
 	}
 
 	@Override
 	protected String getName() {
 		return name;
+	}
+	
+	@Override
+	protected BigDecimal getPrice() {
+		return price;
 	}
 }

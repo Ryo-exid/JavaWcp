08b3945a59cc42430390c6a699136c26cab75cd7
Ryo-exid <sakura4oneok@gmail.com>
2021-09-07 21:18:33 +0900

    関数型インターフェースとラムダ式/ラムダ式を使って hello を実装

---
+++ src/lambda/Lambda01.java
@@ -0,0 +1,14 @@
+package lambda;
+
+public class Lambda01 {
+	public static void main(String[] args) {
+		// ラムダ式を使って hello を実装
+		Greeting greeting = () -> {
+			return "こんにちは";
+		};
+		System.out.println(greeting.hello());
+	}
+}
+
+// ラムダ式
+// (引数) -> {処理};

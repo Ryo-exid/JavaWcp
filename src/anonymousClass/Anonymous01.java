524681a6cdc2a6c0de18b627565a55f185f2c2c7
Ryo-exid <sakura4oneok@gmail.com>
2021-09-07 19:48:04 +0900

    Hello B! を出力する無名クラス

--- src/anonymousClass/Anonymous01.java
+++ src/anonymousClass/Anonymous01.java
@@ -2,6 +2,18 @@ package anonymousClass;
 
 public class Anonymous01 {
 	public static void main(String[] args) {
+		// 通常の実行
+		A a = new A();
+		System.out.println(a.hello());
 		
+		// 無名クラス
+		A b = new A() { // ()のあとに{}を追加
+			// クラスAのhelloメソッドをオーバーライド
+			@Override
+			public String hello() {
+				return "Hello B!";
+			}
+		};
+		System.out.println(b.hello());
 	}
 }
